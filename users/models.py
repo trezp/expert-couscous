726beb8219f353b2aaf2158298f0af63e65e838f
Treasure <treasure.rose@gmail.com>
2018-02-28 20:33:18 -0800

    initial commit

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.contrib.auth.models import AbstractUser, UserManager
+
+# Create your models here.
+class CustomUserManager(UserManager):
+  pass 
+
+class CustomUser(AbstractUser):
+  objects = CustomUserManager()
